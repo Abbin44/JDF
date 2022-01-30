b33a4139254c57c9c008a6571c38683942ddcc93
Abbin44 <abbin44@gmail.com>
2022-01-30 16:22:18 +0100

    Finished all reading/writing functionality

--- JDF/Program.cs
+++ JDF/Program.cs
@@ -11,36 +11,35 @@ namespace JDF
     internal class Program
     {
         static string filePath = string.Empty;
+        static Object[] currentFile;
+        static List<Object> newObjects = new List<Object>();
 
-        static void Main()
-        {
-            filePath = @"F:\Programmering\C#\JDF\JDF\example.jdf";
-            ReadOptions();
-        }
+        //For testing purposes only
+        //static void Main()
+        //{
+        //    filePath = @"F:\Programmering\C#\JDF\JDF\example.jdf";
+        //    currentFile = ReadOptions();
+
+        //    Dictionary<string, string> options = new Dictionary<string, string>();
+        //    options.Add("color", "black");
+        //    AddOption("colors", options);
+        //    WriteNewData();
+        //}
+
         public Program(string path)
         {
             filePath = path;
-            filePath = @"F:\Programmering\C#\JDF\JDF\example.jdf";
+            currentFile = ReadOptions();
         }
 
-        /// <summary>
-        /// Adds an option with a single parameter with a belonging value
-        /// </summary>
-        /// <param name="name">
-        /// The name for the option
-        /// </param>
-        /// <param name="option">
-        /// The name you want to give the parameter
-        /// </param>
-        ///<param name="value">
-        /// The value you want to assign the parameter
-        /// </param>
-        static void AddOptionWithSingleParam(string name, string option, string value)
+        private static bool ContainsName(string name)
         {
-            StringBuilder sb = new StringBuilder();
-            sb.Append(string.Concat(name, "{", "\n"));
-            sb.Append(string.Concat(">", option, "=", value));
-            sb.Append(string.Concat("\n", "}"));
+            for (int i = 0; i < currentFile.Length; ++i)
+            {
+                if (currentFile[i].name == name)
+                    return true;
+            }
+            return false;
         }
 
         /// <summary>
@@ -49,38 +48,31 @@ namespace JDF
         /// <param name="name">
         /// The name for the option
         /// </param>
-        /// <param name="options">
-        /// The names you want to give the parameters
+        /// <param name="parameters">
+        /// A dictionary containing the parameter names and values
         /// </param>
-        ///<param name="values">
-        /// The values you want to assign the parameters
-        /// </param>
-        static void AddOptionWithMultipleParams(string name, string[] options, string[] values)
+        public static void AddOption(string name, Dictionary<string, string> parameters)
         {
-            if (options.Length != values.Length) //Make sure there are as many values as options
-                throw new Exception("Mismatch in option and values count");
+            if (ContainsName(name))
+                throw new Exception("Name already exists, update it instead to avoid doubles.");
 
-            StringBuilder sb = new StringBuilder();
-            sb.Append(string.Concat(name, "{", "\n"));
-            for (int i = 0; i < options.Length; ++i)
-            {
-                sb.Append(string.Concat(">", options[i], "=", values[i], "\n"));
-            }
-            sb.Append("}");
+            Object obj;
+            obj.name = name;
+            obj.parameters = parameters;
+            newObjects.Add(obj);
         }
 
         /// <summary>
         /// Reads all the options from your file and returns them in a list structure
         /// </summary>
-        static Object[] ReadOptions()
+        public static Object[] ReadOptions()
         {
             string objects = File.ReadAllText(filePath);
             int objectCount = Regex.Matches(objects, "}").Count; //This is accurate as long as people follow syntax correctly
             Object[] options = new Object[objectCount];
 
             string[] lines = File.ReadAllLines(filePath);
-
-            Dictionary<string, string> keyValuePairs = new Dictionary<string, string>();
+            Dictionary<string, string> keyValuePairs = null;
 
             string name = string.Empty;
             string[] keys = new string[2];
@@ -88,8 +80,12 @@ namespace JDF
             int objIndex = 0;
             for (int i = 0; i < lines.Length; ++i)
             {
+
                 if (lines[i].Contains("{"))
+                {
                     name = lines[i].Substring(0, lines[i].IndexOf("{")); //Extract the name from the line
+                    keyValuePairs = new Dictionary<string, string>();
+                }
 
                 if (lines[i].StartsWith(">"))
                 {
@@ -110,12 +106,43 @@ namespace JDF
             return options;
         }
 
-        /// <summary>
-        /// Let's you write new data into the JDF file
-        /// </summary>
-        static void WriteNewData(string data)
+        private static string ParseObjects()
         {
-            
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < currentFile.Length; ++i)
+            {
+                sb.Append(string.Concat("\n", currentFile[i].name));
+                sb.Append("{\n");
+                for (int j = 0; j < currentFile[i].parameters.Count; ++j)
+                {
+                    sb.Append(string.Concat(">", currentFile[i].parameters.Keys.ElementAt(j), "=", currentFile[i].parameters.Values.ElementAt(j), "\n"));
+                }
+                sb.Append("}");
+            }
+
+            if (newObjects.Count > 0)
+            {
+                for (int i = 0; i < newObjects.Count; ++i)
+                {
+                    sb.Append(string.Concat("\n", newObjects[i].name));
+                    sb.Append("{\n");
+                    for (int j = 0; j < newObjects[i].parameters.Count; ++j)
+                    {
+                        sb.Append(string.Concat(">", newObjects[i].parameters.Keys.ElementAt(j), "=", newObjects[i].parameters.Values.ElementAt(j), "\n"));
+                    }
+                    sb.Append("}");
+                }
+            }
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Writes all the data that has been added/updated
+        /// </summary>
+        public static void WriteNewData()
+        {
+            string data = ParseObjects();
+            File.WriteAllText(filePath, data);
         }
     }
 }
